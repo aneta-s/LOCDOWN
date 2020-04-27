86c23b7ddd87a0a4ebc36253910ea6c7f696b64f
aneta-s <54153719+aneta-s@users.noreply.github.com>
2020-04-27 11:03:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -21,12 +21,12 @@ function texts(color) {
   ctx.shadowColor = color;
   ctx.fillStyle = color;
   ctx.setTransform(1, -0.15, 0, 1, 0, -10);
-  ctx.fillText("Amsterdown", innerWidth / 2, innerHeight / 2 - 5);
+  ctx.fillText("Gerlines", innerWidth / 2, innerHeight / 2 - 5);
 
   ctx.fillStyle = "white";
   ctx.shadowBlur = 30;
   ctx.shadowColor = color;
-  ctx.fillText("Amsterdown", innerWidth / 2, innerHeight / 2);
+  ctx.fillText("Gerlines", innerWidth / 2, innerHeight / 2);
 
   ctx.font = "8vh Bungee Inline";
   ctx.shadowBlur = 30;
@@ -34,7 +34,7 @@ function texts(color) {
   ctx.fillStyle = "#fff";
   ctx.setTransform(1, -0.15, 0, 1, 0, -10);
   ctx.fillText(
-    "Event 2020",
+    "feestje!",
     innerWidth / 2,
     innerHeight / 2 + innerHeight / 10
   );
